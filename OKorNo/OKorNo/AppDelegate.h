7a363926e449c293a537e1a1aad9961e42947165
qianfeng <qianfeng@fuck.local>
2015-11-23 14:56:12 +0800

    v1

---
+++ OKorNo/OKorNo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OKorNo
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright © 2015年 txn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OKorNo/OKorNo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OKorNo
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright © 2015年 txn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
